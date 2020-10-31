1e2908af27aef21d4f350a92e8f1d82d96cfefa0
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2020-10-31 12:05:36 +0600

    4rd commit

--- Account.cs
+++ Account.cs
@@ -6,6 +6,53 @@ namespace ConsoleApp2
 {
     class Account
     {
+        string accName;
+        string acid;
+        int balance;
+        
 
+        int getBal()
+        {
+            return balance;
+        }
+
+        string getName()
+        {
+            return this.accName;
+        }
+        string getName()
+        {
+            return this.acid;
+        }
+        public void Deposit(int amount)
+        {
+            balance += addAmt;
+        }
+
+        public bool withdraw(double outAmt)
+        {
+            bool chk = true;
+            if (outAmt <= balance)
+            {
+                balance -= outAmt;
+            }
+            else if (outAmt > balance)
+            {
+                chk = false;
+            }
+            return chk;
+        }
+
+        public int getBalAfter(int mos)
+        {
+            double prin = balance;
+            double intFeed;
+            for (int g = 1; g <= mos; g++)
+            {
+                intFeed = prin * interest;
+                prin += intFeed;
+            }
+            return prin;
+        }
     }
 }
--- Course.cs
+++ Course.cs
@@ -6,5 +6,39 @@ namespace ConsoleApp2
 {
     class Course
     {
+        string courseName;
+        string courseCode;
+        int courseCredit;
+        string CourseName
+        {
+            set { courseName = value; }
+            get { return courseName; }
+        }
+        string CourseCode
+        {
+            set { courseCode = value; }
+            get { return courseCodee; }
+        }
+
+        int CourseCredit
+        {
+            set { courseCredit courseCredit = value; }
+            get { return courseCredit; }
+        }
+        public Course()
+        {
+        }
+        public Course(string courseName, string courseCode, int courseCredit)
+        {
+            this.courseName = courseName;
+            this.courseCode = courseCode;
+            this.courseCredit = courseCredit;
+        }
+        void ShowInfo()
+        {
+            CourseName = "C#";
+            CourseCode = "Rahim";
+            CourseCredit = 3;
+        }
     }
 }
